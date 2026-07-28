b9ada155e4033f1790943cce7415fe6937646804
HayC595 <hailey.bug.cross@gmail.com>
2026-07-28 21:05:34 +0000

    Start Done

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Chess-I-Guess");
+        System.out.println("Please Press Enter to Start");
+        scanner.nextLine();
+        System.out.println("Game Starting...");
+    }
+}
