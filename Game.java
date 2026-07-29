a3af6e0792450bf25fd17ef6ce3dcdb1bb9fb858
HayC595 <hailey.bug.cross@gmail.com>
2026-07-29 01:00:08 +0000

    Pieces Added

--- Game.java
+++ Game.java
@@ -1,20 +1,13 @@
 public class Game extends Main {
 
     public static final String RESET = "\u001B[38;2;245;245;180m"; //Norm Text Color
+    public static final String NORM = "\u001B[m"; // Normal Background Color
     public static final String GREEN = "\u001B[32m"; // Green Pieces
     public static final String RED = "\u001B[31m"; // Instruction Text Color
     public static final String WHITE = "\u001B[97m"; // White Pieces
     public static final String BLUE = "\u001B[34m"; // Barriers
     public static final String OFF_WHITE_BACKGROUND = "\u001B[48;2;245;245;180m"; // Off-White Background
     public static final String BLACK_BACKGROUND = "\u001B[40m"; // Black Background
-
-    public static void main(String[] args) {
-    // Background black, text white
-    System.out.println(BLACK_BACKGROUND + WHITE + "This is White text on Black background" + RESET);
-
-    // Background yellow, text red
-    System.out.println(OFF_WHITE_BACKGROUND + GREEN + "This is Green text on Off-White background" + RESET);
-    }
 
     public static String[][] GameBoard;
 
@@ -23,10 +16,35 @@ public class Game extends Main {
         GameBoard = new String[8][8];
 
         for (int r = 0; r < 8; r++) {
-            for (int c = 0; c < 8; c++) {
-                GameBoard[r][c] = ".";
-            }
+            GameBoard[1][r] = GREEN + " P " + RESET;
+        }
+        for (int r = 0; r < 8; r++) {
+            GameBoard[6][r] = WHITE + " P " + RESET;
+        }
 
+        GameBoard[0][0] = GREEN + " R " + RESET;
+        GameBoard[0][7] = GREEN + " R " + RESET;
+        GameBoard[7][0] = WHITE + " R " + RESET;
+        GameBoard[7][7] = WHITE + " R " + RESET;
+        GameBoard[0][1] = GREEN + " N " + RESET;
+        GameBoard[0][6] = GREEN + " N " + RESET;
+        GameBoard[7][1] = WHITE + " N " + RESET;
+        GameBoard[7][6] = WHITE + " N " + RESET;
+        GameBoard[0][2] = GREEN + " B " + RESET;
+        GameBoard[0][5] = GREEN + " B " + RESET;
+        GameBoard[7][2] = WHITE + " B " + RESET;
+        GameBoard[7][5] = WHITE + " B " + RESET;
+        GameBoard[0][3] = GREEN + " Q " + RESET;
+        GameBoard[7][3] = WHITE + " Q " + RESET;
+        GameBoard[0][4] = GREEN + " K " + RESET;
+        GameBoard[7][4] = WHITE + " K " + RESET;
+
+        for(int r = 0; r < 8; r++) {
+            for(int c = 0; c < 8; c++) {
+                if(GameBoard[r][c] == null){
+                    GameBoard[r][c] = "   ";
+                }
+            }
         }
     }
 
@@ -38,7 +56,7 @@ public class Game extends Main {
             } else {
                 System.out.print(BLACK_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][0]);
+                System.out.print(GameBoard[r][0]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -47,7 +65,7 @@ public class Game extends Main {
             } else {
                 System.out.print(OFF_WHITE_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][1]);
+                System.out.print(GameBoard[r][1]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -56,7 +74,7 @@ public class Game extends Main {
             } else {
                 System.out.print(BLACK_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][2]);
+                System.out.print(GameBoard[r][2]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -65,7 +83,7 @@ public class Game extends Main {
             } else {
                 System.out.print(OFF_WHITE_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][3]);
+                System.out.print(GameBoard[r][3]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -74,7 +92,7 @@ public class Game extends Main {
             } else {
                 System.out.print(BLACK_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][4]);
+                System.out.print(GameBoard[r][4]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -83,7 +101,7 @@ public class Game extends Main {
             } else {
                 System.out.print(OFF_WHITE_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][5]);
+                System.out.print(GameBoard[r][5]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -92,7 +110,7 @@ public class Game extends Main {
             } else {
                 System.out.print(BLACK_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][6]);
+                System.out.print(GameBoard[r][6]);
         }
         System.out.println();
         for (int r = 0; r < 8; r++) {
@@ -101,8 +119,8 @@ public class Game extends Main {
             } else {
                 System.out.print(OFF_WHITE_BACKGROUND);
             }
-                System.out.print(RESET + GameBoard[r][7]);
+                System.out.print(GameBoard[r][7]);
         }
-
+        System.out.println(NORM);
     }
 }
