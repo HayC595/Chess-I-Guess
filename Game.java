0cb907de3dd8ef00f454d368045151eb80b9c701
HayC595 <hailey.bug.cross@gmail.com>
2026-07-29 00:36:21 +0000

    Made Board

--- Game.java
+++ Game.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
-
 public class Game extends Main {
 
-    public static final String RESET = "\u001B[38;2;245;245;180m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String RED = "\u001B[31m";
-    public static final String WHITE = "\u001B[97m";
-    public static final String OFF_WHITE_BACKGROUND = "\u001B[48;2;245;245;180m";
-
-    public static final String BLACK_BACKGROUND = "\u001B[40m";
+    public static final String RESET = "\u001B[38;2;245;245;180m"; //Norm Text Color
+    public static final String GREEN = "\u001B[32m"; // Green Pieces
+    public static final String RED = "\u001B[31m"; // Instruction Text Color
+    public static final String WHITE = "\u001B[97m"; // White Pieces
+    public static final String BLUE = "\u001B[34m"; // Barriers
+    public static final String OFF_WHITE_BACKGROUND = "\u001B[48;2;245;245;180m"; // Off-White Background
+    public static final String BLACK_BACKGROUND = "\u001B[40m"; // Black Background
 
     public static void main(String[] args) {
     // Background black, text white
     System.out.println(BLACK_BACKGROUND + WHITE + "This is White text on Black background" + RESET);
 
     // Background yellow, text red
-    System.out.println(RESET + GREEN + "This is Green text on Off-White background" + RESET);
+    System.out.println(OFF_WHITE_BACKGROUND + GREEN + "This is Green text on Off-White background" + RESET);
     }
 
     public static String[][] GameBoard;
 
-    public static void game(String[] args){
+    public static void BoardSetup(){
 
         GameBoard = new String[8][8];
 
@@ -30,5 +28,81 @@ public class Game extends Main {
             }
 
         }
+    }
+
+    public static void PrintBoard(){
+
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            } else {
+                System.out.print(BLACK_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][0]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(BLACK_BACKGROUND);
+            } else {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][1]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            } else {
+                System.out.print(BLACK_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][2]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(BLACK_BACKGROUND);
+            } else {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][3]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            } else {
+                System.out.print(BLACK_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][4]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(BLACK_BACKGROUND);
+            } else {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][5]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            } else {
+                System.out.print(BLACK_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][6]);
+        }
+        System.out.println();
+        for (int r = 0; r < 8; r++) {
+            if (r % 2 == 0) {
+                System.out.print(BLACK_BACKGROUND);
+            } else {
+                System.out.print(OFF_WHITE_BACKGROUND);
+            }
+                System.out.print(RESET + GameBoard[r][7]);
+        }
+
     }
 }
--- Main.java
+++ Main.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
 
-        Game game = new Game();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(Game.RESET + "Welcome to Chess I Guess");
         System.out.println(Game.RESET + "This is a Two Player Chess Game (No AI)");
         System.out.println(Game.RESET + "The Game is played with" + Game.WHITE + " WHITE " + Game.RESET + "and" + Game.GREEN + " GREEN " + Game.RESET + "pieces");
+        System.out.println(Game.BLUE + "---------------------------||");
+        System.out.println(Game.RESET + "The Peices are as follows: ");
+        System.out.println(Game.RESET + "P - Pawn");
+        System.out.println(Game.RESET + "R - Rook");
+        System.out.println(Game.RESET + "N - Knight");
+        System.out.println(Game.RESET + "B - Bishop");
+        System.out.println(Game.RESET + "Q - Queen");
+        System.out.println(Game.RESET + "K - King");
+        System.out.println(Game.BLUE + "---------------------------||");      
         System.out.println(Game.RED + "Please Press Enter to Start" + Game.RESET);
         scanner.nextLine();
         System.out.println(Game.RESET + "Game Starting...");
-        
-        //game.game(args);
+
+        Game.BoardSetup();
+        Game.PrintBoard();
     }
 }
